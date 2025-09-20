3e183078d3e1bb068d420a6bd040fc007888523e
Nova <idk77780011@gmail.com>
2025-09-20 22:54:09 +0100

    Create Plugin.cs

---
+++ Plugin.cs
@@ -0,0 +1,15 @@
+using BepInEx;
+using Photon.Pun;
+using System.IO;
+
+namespace GorillaMod
+{
+	[BepInPlugin("com.nova.gorillamod", "Gorilla Mod", "1.0.0")]
+	public class Plugin : BaseUnityPlugin
+	{
+		public void Awake()
+		{
+			File.WriteAllLines(Path.Combine(Paths.PluginPath, "RPCS.txt"), PhotonNetwork.PhotonServerSettings.RpcList.ToArray());
+		}
+	}
+}
